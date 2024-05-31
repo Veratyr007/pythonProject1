eb212fb02e6ea2cfab1988529f656c6d1b60469a
Veratyr007 <peony.blossom007@gmail.com>
2024-05-31 15:33:29 +0200

    initial

---
+++ main.py
@@ -0,0 +1,18 @@
+my_dict = {"Name": "Anna", "Country": "Russia", "Age": 30 }
+print(my_dict)
+print(my_dict["Country"])
+print(my_dict.get("Unicorn"))
+my_dict.update({"languages": "English and Spanish", "fav number": 7} )
+a = my_dict.pop("Age")
+print(a)
+print(my_dict)
+
+my_set = {"pancake", 17, "tart", 17,"tart", "chocolate", 10, "pancake"}
+print(my_set)
+my_set.add("coffee")
+my_set.add(25)
+my_set.remove("pancake")
+print(my_set)
+
+
+
